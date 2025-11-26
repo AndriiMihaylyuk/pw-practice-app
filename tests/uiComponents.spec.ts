71e2eab8168a0bd6b82e7503691b9fd1753eac51
Andrew Mihaylyuk <tobinoja@gmail.com>
2025-11-26 17:15:22 +0200

    first commit

---
+++ tests/uiComponents.spec.ts
@@ -0,0 +1,145 @@
+import { test, expect } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://localhost:4200/");
+});
+
+test.describe("Form Layouts page", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByText("Forms").click();
+    await page.getByText("Form Layouts").click();
+  });
+
+  test("input fields", async ({ page }) => {
+    const usingTheGridEmailInput = page
+      .locator("nb-child", { hasText: "Using the Grid" })
+      .getByRole("textbox", { name: "Email" });
+
+    await usingTheGridEmailInput.fill("test@test,com");
+    await usingTheGridEmailInput.clear();
+    await usingTheGridEmailInput.pressSequentially("test2@test.com", {
+      delay: 500,
+    });
+
+    // generic assertion
+    const inputValue = await usingTheGridEmailInput.inputValue();
+    expect(inputValue).toEqual("test2@test.com");
+
+    // locator assertion
+    await expect(usingTheGridEmailInput).toHaveValue("test2@test.com");
+  });
+
+  test("radio buttons", async ({ page }) => {
+    const usingTheGridForm = page.locator("nb-card", {
+      hasText: "Using the Grid",
+    });
+
+    // await usingTheGridForm.getByLabel('Option 1').check({force: true});
+    await usingTheGridForm
+      .getByRole("radio", { name: "Option 1" })
+      .check({ force: true });
+    // generic assertion
+    const radioStatus = await usingTheGridForm
+      .getByRole("radio", { name: "Option 1" })
+      .isChecked();
+    expect(radioStatus).toBeTruthy();
+
+    // locator assertion
+    await expect(
+      usingTheGridForm.getByRole("radio", { name: "Option 1" })
+    ).toBeChecked();
+
+    await usingTheGridForm
+      .getByRole("radio", { name: "Option 2" })
+      .check({ force: true });
+    expect(
+      await usingTheGridForm
+        .getByRole("radio", { name: "Option 1" })
+        .isChecked()
+    ).toBeFalsy();
+    expect(
+      await usingTheGridForm
+        .getByRole("radio", { name: "Option 2" })
+        .isChecked()
+    ).toBeTruthy();
+  });
+});
+
+test("checkboxes", async ({ page }) => {
+  await page.getByText("Modal & Overlays").click();
+  await page.getByText("Toastr").click();
+
+  await page
+    .getByRole("checkbox", { name: "Hide on click" })
+    .check({ force: true });
+  await page
+    .getByRole("checkbox", { name: "Hide on click" })
+    .uncheck({ force: true });
+
+  const allBoxes = page.getByRole("checkbox");
+  for (const box of await allBoxes.all()) {
+    // await box.check({force: true});
+    // expect(await box.isChecked()).toBeTruthy();
+    await box.uncheck({ force: true });
+    expect(await box.isChecked()).toBeFalsy();
+  }
+});
+
+test("lists and dropdowns", async ({ page }) => {
+  const dropDownMenu = page.locator("ngx-header nb-select");
+  await dropDownMenu.click();
+
+  page.getByRole("list"); //when the list has a UL tag
+  page.getByRole("listitem"); //when the list has LI tag
+
+  const optionList = page.locator("nb-option-list nb-option");
+  await expect(optionList).toHaveText(["Light", "Dark", "Cosmic", "Corporate"]);
+  await optionList.filter({ hasText: "Cosmic" }).click();
+  const header = page.locator("nb-layout-header");
+  await expect(header).toHaveCSS("background-color", "rgb(50, 50, 89)");
+
+  const colors = {
+    Light: "rgb(255, 255, 255)",
+    Dark: "rgb(34, 43, 69)",
+    Cosmic: "rgb(50, 50, 89)",
+    Corporate: "rgb(255, 255, 255)",
+  };
+
+  await dropDownMenu.click();
+  for (const color in colors) {
+    await optionList.filter({ hasText: color }).click();
+    await expect(header).toHaveCSS("background-color", colors[color]);
+    if (color !== "Corporate") {
+      await dropDownMenu.click();
+    }
+  }
+});
+
+test("tooltips", async ({ page }) => {
+  // Go ot Source tab -> click Cmd + \ to freeze the screen -> inspect a tooltip in the Elements tab:
+  await page.getByText("Modal & Overlays").click();
+  await page.getByText("Tooltip").click();
+
+  const toolTipCard = page.locator("nb-card", {
+    hasText: "Tooltip Placements",
+  });
+  await toolTipCard.getByRole("button", { name: "Top" }).hover();
+
+  page.getByRole("tooltip"); //if you have a role tooltip created
+  const tooltip = await page.locator("nb-tooltip").textContent();
+  expect(tooltip).toEqual("This is a tooltip on top!");
+});
+
+test("dialog boxes", async ({ page }) => {
+  await page.getByText("Modal & Overlays").click();
+  await page.getByText("Smart Table").click();
+
+ page.on('dialog', dialog =>{
+    expect(dialog.message()).toEqual('Are you sure you want to delete?');
+    dialog.accept();
+ });
+
+ await page.getByRole('table').locator('tr', {hasText: 'mdo@gmail.com'}).locator('.nb-trash').click();
+ await expect(page.locator('table tr').first()).not.toHaveText('mdo@gmail.com');
+});
+
